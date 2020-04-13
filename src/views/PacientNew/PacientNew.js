6161b4f69e8f14674938e9bc4997bbdf6d0f6104
Andre Seiji Tamanaha <a.seiji.t@gmail.com>
2020-04-13 20:10:31 -0300

    feat: basic fields for create pacient

--- src/views/PacientNew/PacientNew.js
+++ src/views/PacientNew/PacientNew.js
@@ -87,6 +87,254 @@ const PacientNew = ({ history }) => {
             </div>
           )}
           <form onSubmit={handleSubmit}>
+            <h4 className="title is-4">Dados básicos</h4>
+            <div className="columns">
+              <div className="field column">
+                <label className="label">CPF</label>
+                <div className="control">
+                  <input className="input" type="text" placeholder="XXX.XXX.XXX-XX" onChange={(e) => setCPF(e.target.value)} disabled={loading} />
+                </div>
+              </div>
+              <div className="field column">
+                <label className="label">Nome do paciente</label>
+                <div className="control">
+                  <input className="input" type="text" onChange={(e) => setPacientName(e.target.value)} disabled={loading} />
+                </div>
+              </div>
+              <div className="field column">
+                <label className="label">Nome da mãe</label>
+                <div className="control">
+                  <input className="input" type="text" onChange={(e) => setMotherName(e.target.value)} disabled={loading} />
+                </div>
+              </div>
+            </div>
+            <div className="columns">
+              <div className="field column is-one-third">
+                <label className="label">Telefone</label>
+                <div className="control">
+                  <input className="input" type="text" onChange={(e) => setPhoneNumber(e.target.value)} disabled={loading} />
+                </div>
+              </div>
+              <div className="field column is-one-third">
+                <label className="label">Data de nascimento</label>
+                <div className="control">
+                  <input className="input" type="text" onChange={(e) => setBirthdate(e.target.value)} disabled={loading} />
+                </div>
+              </div>
+            </div>
+            <div className="columns">
+              <div className="field column is-one-third">
+                <label className="label">Orientação sexual</label>
+                <div className="control">
+                  <input className="input" type="text" onChange={(e) => setSexOrientation(e.target.value)} disabled={loading} />
+                </div>
+              </div>
+              <div className="field column">
+                <label className="label">Sexo</label>
+                <div className="control">
+                  <div className="select">
+                    <select onChange={(e) => setSex(e.target.value)} disabled={loading}>
+                      <option disabled>Selecione...</option>
+                      <option value="F">Feminino</option>
+                      <option value="M">Masculino</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <hr />
+            <h4 className="title is-4">Endereço</h4>
+            <div className="columns">
+              <div className="field column">
+                <label className="label">Rua</label>
+                <div className="control">
+                  <input
+                    className="input"
+                    type="text"
+                    onChange={
+                      (e) => setAddress((addr) => ({
+                        ...addr,
+                        street: e.target.value
+                      }))
+                    }
+                    disabled={loading}
+                  />
+                </div>
+              </div>
+              <div className="field column">
+                <label className="label">Número</label>
+                <div className="control">
+                  <input
+                    className="input"
+                    type="text"
+                    onChange={
+                      (e) => setAddress((addr) => ({
+                        ...addr,
+                        number: e.target.value
+                      }))
+                    }
+                    disabled={loading}
+                  />
+                </div>
+              </div>
+              <div className="field column">
+                <label className="label">Bairro</label>
+                <div className="control">
+                  <input
+                    className="input"
+                    type="text"
+                    onChange={
+                      (e) => setAddress((addr) => ({
+                        ...addr,
+                        neighborhood: e.target.value
+                      }))
+                    }
+                    disabled={loading}
+                  />
+                </div>
+              </div>
+              <div className="field column">
+                <label className="label">Unidade de referência</label>
+                <div className="control">
+                  <input
+                    className="input"
+                    type="text"
+                    onChange={
+                      (e) => setAddress((addr) => ({
+                        ...addr,
+                        reference_unit: e.target.value
+                      }))
+                    }
+                    disabled={loading}
+                  />
+                </div>
+              </div>
+            </div>
+            <hr />
+            <h4 className="title is-4">Relato</h4>
+            <div className="columns">
+              <div className="field column">
+                <label className="label">Origem do dado</label>
+                <div className="control">
+                  <input
+                    className="input"
+                    type="text"
+                    onChange={
+                      (e) => setReport((rep) => ({
+                        ...rep,
+                        data_origin: e.target.value
+                      }))
+                    }
+                    disabled={loading}
+                  />
+                </div>
+              </div>
+              <div className="field column">
+                <label className="label">Comorbidade</label>
+                <div className="control">
+                  <input
+                    className="input"
+                    type="text"
+                    onChange={
+                      (e) => setReport((rep) => ({
+                        ...rep,
+                        comorbidity: e.target.value
+                      }))
+                    }
+                    disabled={loading}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="columns">
+              <div className="field column is-2">
+                <label className="label">Exame Covid</label>
+                <div className="control">
+                  <div className="select">
+                    <select
+                      onChange={
+                        (e) => setReport((rep) => ({
+                          ...rep,
+                          covid_exam: e.target.value
+                        }))
+                      }
+                    >
+                      <option disabled>Selecione...</option>
+                      <option value>Sim</option>
+                      <option value={false}>Não</option>
+                    </select>
+                  </div>
+                </div>
+              </div>
+              <div className="field column">
+                <label className="label">Resultado do exame</label>
+                <div className="control">
+                  <input
+                    className="input"
+                    type="text"
+                    onChange={
+                      (e) => setReport((rep) => ({
+                        ...rep,
+                        covid_result: e.target.value
+                      }))
+                    }
+                    disabled={loading}
+                  />
+                </div>
+              </div>
+              <div className="field column">
+                <label className="label">Situação</label>
+                <div className="control">
+                  <input
+                    className="input"
+                    type="text"
+                    onChange={
+                      (e) => setReport((rep) => ({
+                        ...rep,
+                        situation: e.target.value
+                      }))
+                    }
+                    disabled={loading}
+                  />
+                </div>
+              </div>
+              <div className="field column">
+                <label className="label">Data de notificação</label>
+                <div className="control">
+                  <input
+                    className="input"
+                    type="text"
+                    onChange={
+                      (e) => setReport((rep) => ({
+                        ...rep,
+                        notification_date: e.target.value
+                      }))
+                    }
+                    disabled={loading}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="columns">
+              <div className="field column">
+                <label className="label">Sintomas</label>
+                <div className="control">
+                  <input
+                    className="input"
+                    type="text"
+                    onChange={
+                      (e) => setReport((rep) => ({
+                        ...rep,
+                        symptoms: e.target.value
+                      }))
+                    }
+                    disabled={loading}
+                  />
+                </div>
+                <p className="help">Separe com vírgulas</p>
+              </div>
+            </div>
+            <hr />
             <div className="field">
               <p className="control button-container">
                 <button type="submit" className="button is-success" disabled={loading}>
